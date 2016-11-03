38b128a91bb5c1cbcb5c164ba1444d970ef1855d
shaelynrich <ric13011@byui.edu>
2016-11-03 16:42:33 -0600

    update
    adding modularization

---
+++ homework/modules/modules.js
@@ -0,0 +1,8 @@
+//A jQuery fundtion to implement modularization
+
+$(function () {
+  $("header").load("../homework/modules/header.html");
+  $("nav").load("../homework/modules/nav.html");
+  $("footer").load("../homework/modules/footer.html");
+  $("#wireframes").load("../homework/modules/wireframes.html");
+});
